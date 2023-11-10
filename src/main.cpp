da8a7b8156f42c2859092bbe602d937f69a3f350
Oren Bell <oren.bell@wustl.edu>
2023-11-10 14:54:41 -0600

    Generate UUID in controller

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,15 @@
 #include <WiFiClient.h>
 #include <Wire.h>
 #include <ArduinoJson.h>
+#include <EEPROM.h>
 
 #include "mapping.h"
 
 #define SSID "ssid"
 #define PASSWORD "password"
 #define SERVER "http://192.168.0.1/"
+
+#define EEPROM_SIZE 512
 
 String uuid = "";
 int intersection_id = -1;
@@ -36,39 +39,88 @@ void set_traffic_light(int addr, int state) {
   Wire.endTransmission();
 }
 
-int enter_state(int led_mask, int blink_mask, int blink_duration, int state_duration, int interrupt_mask) {
-    int time = 0;
-    int interrupt_state = 0;
+// int enter_state(int led_mask, int blink_mask, int blink_duration, int state_duration, int interrupt_mask) {
+//     int time = 0;
+//     int interrupt_state = 0;
     
-    // Do not blink
-    if (blink_duration <= 0) {
-        blink_duration = state_duration;
+//     // Do not blink
+//     if (blink_duration <= 0) {
+//         blink_duration = state_duration;
+//     }
+
+//     set_traffic_light(NE_CORNER_ADDR, led_mask);
+//     set_traffic_light(NW_CORNER_ADDR, led_mask);
+//     set_traffic_light(SW_CORNER_ADDR, led_mask);
+//     set_traffic_light(SE_CORNER_ADDR, led_mask);
+
+//     while(time < state_duration && !(interrupt_state & interrupt_mask)) {
+//         delay(100);
+//         time++;
+
+//         if (time % blink_duration == 0) {
+//             led_mask ^= blink_mask;
+//             set_traffic_light(NE_CORNER_ADDR, led_mask);
+//             set_traffic_light(NW_CORNER_ADDR, led_mask);
+//             set_traffic_light(SW_CORNER_ADDR, led_mask);
+//             set_traffic_light(SE_CORNER_ADDR, led_mask);
+//         }
+
+//         interrupt_state &= interrupt_mask;
+
+//         if (interrupt_state) {
+//             break;
+//         }
+//     }
+//     return interrupt_state;
+// }
+
+int enter_state(int state, int duration, int interrupt_mask) {
+  int led_mask, blink_mask, time = 0, interrupt_state = 0;
+  switch(state) {
+    case 0:
+      led_mask = GREEN_LIGHT | WALK | DNW_PERP;
+      blink_mask = 0;
+      break;
+    case 1:
+      led_mask = GREEN_LIGHT | DNW | DNW_PERP;
+      blink_mask = DNW;
+      break;
+    case 2:
+      led_mask = YELLOW_LIGHT | DNW | DNW_PERP;
+      blink_mask = 0;
+      break;
+    case 3:
+      led_mask = RED_LIGHT | DNW | DNW_PERP;
+      blink_mask = 0;
+      break;
+    case 4:
+      led_mask = RED_LIGHT | DNW | WALK_PERP;
+      blink_mask = 0;
+      break;
+    case 5: 
+      led_mask = RED_LIGHT | DNW | DNW_PERP;
+      blink_mask = DNW_PERP;
+      break;
+    case 6:
+      led_mask = RED_LIGHT | DNW | DNW_PERP;
+      blink_mask = 0;
+      break;
+    default:
+      led_mask = RED_LIGHT | WALK; // Impossible state
+      blink_mask = RED_LIGHT;
+      break;
+  }
+
+  set_traffic_light(NE_CORNER_ADDR, led_mask);
+
+  while(time < duration && !(interrupt_state & interrupt_mask)) {
+    delay(100);
+    time+=100;
+
+    if (time % 800 == 0) {
+      led_mask ^= blink_mask;
+      set_traffic_light(NE_CORNER_ADDR, led_mask);
     }
-
-    set_traffic_light(NE_CORNER_ADDR, led_mask);
-    set_traffic_light(NW_CORNER_ADDR, led_mask);
-    set_traffic_light(SW_CORNER_ADDR, led_mask);
-    set_traffic_light(SE_CORNER_ADDR, led_mask);
-
-    while(time < state_duration && !(interrupt_state & interrupt_mask)) {
-        delay(100);
-        time++;
-
-        if (time % blink_duration == 0) {
-            led_mask ^= blink_mask;
-            set_traffic_light(NE_CORNER_ADDR, led_mask);
-            set_traffic_light(NW_CORNER_ADDR, led_mask);
-            set_traffic_light(SW_CORNER_ADDR, led_mask);
-            set_traffic_light(SE_CORNER_ADDR, led_mask);
-        }
-
-        interrupt_state &= interrupt_mask;
-
-        if (interrupt_state) {
-            break;
-        }
-    }
-    return interrupt_state;
 }
 
 void led_test() {
@@ -105,47 +157,25 @@ void setup() {
     Wire.beginTransmission(NE_CORNER_ADDR);
     Wire.write(buf, 11);
     Wire.endTransmission();
+
+    EEPROM.begin(EEPROM_SIZE);
+    
+    // If fresh device, generate a uuid
+    if (EEPROM.read(0) == 0x01) {
+      uuid = EEPROM.readString(1, 36);
+    } else {
+      uuid = "";
+      for (int i = 0; i < 36; i++) {
+        uuid += String(random(16), HEX);
+      }
+      EEPROM.write(0, 0x01);
+      EEPROM.writeString(1, uuid);
+      EEPROM.commit();
+    }
 }
 
 void loop() {
     led_test();
-
-    // TODO: 
-
-    // If uuid is not set, get it from the server from call to server at /getLightID
-    if (strlen(uuid) == 0) {
-        HTTPClient http;
-        http.begin(SERVER + "getLightID")
-        int httpCode = http.GET();
-        if (httpCode > 0) {
-            String payload = http.getString();
-            Serial.println(payload);
-            DynamicJsonDocument doc(1024);
-            deserializeJson(doc, payload);
-            uuid = doc["uuid"].as<String>();
-            intersection_id = doc["intersection_id"].as<int>();
-            intersection_location = doc["intersection_location"].as<String>();
-        }
-        delay(1000);
-        return;
-    }
-
-    // If uuid is set, but location isn't known, get it from the server at /getLightLocation/<uuid>
-    if(intersection_id == -1) {
-        HTTPClient http;
-        http.begin(SERVER + "getLightLocation/" + uuid);
-        int httpCode = http.GET();
-        if (httpCode > 0) {
-            String payload = http.getString();
-            Serial.println(payload);
-            DynamicJsonDocument doc(1024);
-            deserializeJson(doc, payload);
-            intersection_id = doc["intersection_id"].as<int>();
-            intersection_location = doc["intersection_location"].as<String>();
-        }
-        delay(1000);
-        return;
-    }
 
     // If uuid and location are known, get the state from the server at /getLightState/<uuid>
     HTTPClient http;
@@ -157,16 +187,16 @@ void loop() {
         DynamicJsonDocument doc(1024);
         deserializeJson(doc, payload);
         int state = doc["state"].as<int>();
-        int state_duration = doc["state_duration"].as<int>();
+        int remaining_ms = doc["remaining_ms"].as<int>();
         int blink_duration = doc["blink_duration"].as<int>();
-        int interrupt_mask = doc["interrupt_mask"].as<int>();
-        int interrupt_state = enter_state(state, 0, blink_duration, state_duration, interrupt_mask);
-        if (interrupt_state & interrupt_mask) {
-            http.begin(SERVER + "setLightState/" + uuid + "/" + interrupt_state);
-            http.GET();
-        }
+        // int interrupt_mask = doc["interrupt_mask"].as<int>();
+        int interrupt_state = enter_state(state, remaining_ms, 0);
+        // if (interrupt_state & interrupt_mask) {
+        //     http.begin(SERVER + "setLightState/" + uuid + "/" + interrupt_state);
+        //     http.GET();
+        // }
     } else if (httpCode == 401) {
-        // If the server returns 401, the light is not registered, so we need to register it
+        // If the server returns 401, the light is not registered. Do nothing
         uuid = "";
         intersection_id = -1;
         intersection_location = "";
